52cafd595262aa4fc4b28afcd333c44968370c48
htmlou <backuppluiza@gmail.com>
2023-09-16 19:23:52 -0300

    last

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ from tkinter import *
 from time import strftime
 
 root = Tk ()
-root.tittle("Relógio DIgital")
+root.title("Relógio DIgital")
 
 def time():
     string = strftime ("%H:%M:%S")
     Label.config(text=string)
     Label.after(1000, time)
+
+Label = Label (root,font=("ds-digital", 80), background="black", foreground="purple")
+Label.pack(anchor="center")
+time()
+
+mainloop()
